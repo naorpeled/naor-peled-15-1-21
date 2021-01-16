8d0dd41ef983d61593090ad36ea4f346bd62880d
Naor Peled <thehecticbyte@gmail.com>
2021-01-16 18:42:08 +0200

    feat(security): add helmet and enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors();
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
